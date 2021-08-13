d08171bb1c43f7780c9982a57850c58095096786
kryptocodes <srivatsantb@gmail.com>
2021-08-13 09:07:35 +0530

    initial commit
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../../logo.png'
+
+const NavBar = () => {
+    return (
+        <div className="navbar">
+            <span className="navbar-brand  mx-auto d-block">
+            <img src={logo} width="40%" className="img-fluid  mx-auto d-block "/>
+            </span>
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+export default NavBar
