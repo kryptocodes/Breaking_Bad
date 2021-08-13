155db529d24296d2006378ceb458c22a6f21d798
kryptocodes <srivatsantb@gmail.com>
2021-08-13 11:58:36 +0530

    added search and card
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,40 +2,53 @@ import React, { useEffect, useState } from 'react'
 import NavBar from './components/navBar/NavBar'
 
 import axios from 'axios'
+import Card from './components/layout/card'
 
 export default function App() {
 
   const [more, setMore] = useState(0)
   const [data, setData] = useState({isLoading: true, data: null})
+  const [Filter, setFilter] = useState('')
+  console.log(Filter)
   useEffect(() => {
+    Filter !== '' ? setMore(0) : setMore(more);
     setData({isLoading: false })
-    axios.get('https://www.breakingbadapi.com/api/characters?limit=10&offset=' + more)
+    axios.get(`https://www.breakingbadapi.com/api/characters?name=${Filter}&limit=10&offset=${more}`)
       .then(response => {
           setData({isLoading: false, data:response?.data})
       })
-  }, [more])
+  }, [more,Filter])
+
+
   return (
 
     <div>
       <NavBar/>
       <div className="container">
+  
         <div className="row">
+            <div className="col-md-6">
             <h1>Breaking Bad Characters</h1>
-            <div className="list">
+            </div>
+            <div className="col-md-6">
+            <input type="text" placeholder="Search" className="w-100 input-form" onChange={(e) => setFilter(e.target.value)} />
+            </div>
+            </div>
+            <div className="row">
               {data?.data?.map((v,i) => (
-                <div key={i} className="item">
-                  <h2>{v.name}</h2>
+               <div className="col-md-3 p-4">
+                  <Card data={v}/>
                 </div>
               ))}
             </div>
+        
             <nav aria-label="Page navigation example">
   <ul className="pagination">
-    <li className="page-item"><a class="page-link" onClick={() => more !== 0 && setMore(more-10)} href="#">Previous</a></li>
-    <li className="page-item"><a class="page-link" onClick={() => more !== 60 && setMore(more+10)}>Next</a></li>
+    <li className="page-item"><button class="page-link" onClick={() => more !== 0 && setMore(more-10)} >Previous</button></li>
+    <li className="page-item"><button class="page-link" onClick={() => more !== 60 && setMore(more+10)}>Next</button></li>
   </ul>
 </nav>
         </div>
         </div>
-    </div>
   )
 }
--- src/components/layout/card.jsx
+++ src/components/layout/card.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-export default function card() {
-    return (
-        <div>
-            
-        </div>
-    )
+
+const Card = ({ data }) => {
+  return (
+    <div className="card bg-dark text-white shadow-lg rounded-2 h-100" >
+      <img src={data?.img} className="img-fluid figure-img rounded" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{data?.name}</h5>
+        <p className="card-text"><span className="badge bg-secondary">Status:</span> {data?.status}</p>
+        <p className="card-text"><span className="badge bg-secondary">NickName:</span> {data?.nickname}</p>
+      </div>
+    </div>
+  )
 }
+
+
+
+
+
+
+
+
+
+export default Card
